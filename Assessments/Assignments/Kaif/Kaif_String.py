6d148d54b31c61183337c7527a336b2d563d49d4
CM-HARISH <101856776+CM-HARISH@users.noreply.github.com>
2022-09-19 14:28:00 +0530

    Create Kaif_String.py

---
+++ Assessments/Assignments/Kaif/Kaif_String.py
@@ -0,0 +1,10 @@
+n1="Mohammed"
+n2="Kaif"
+a=n1+n2
+print("concatenated string is:", a)
+
+str=a
+print("Reversed string is:", str[::-1])
+
+str='Mohammedkaifk'
+print("Sliced string is:", str[9:14])

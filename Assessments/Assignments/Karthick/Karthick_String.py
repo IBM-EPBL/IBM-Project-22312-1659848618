1b7b44dc9d5e42ea77fc3a95fbbc5b57ffb81e74
CM-HARISH <101856776+CM-HARISH@users.noreply.github.com>
2022-09-19 14:33:38 +0530

    Create Karthick_String.py

---
+++ Assessments/Assignments/Karthick/Karthick_String.py
@@ -0,0 +1,10 @@
+n1="Karthick"
+n2="Venkatesan"
+a=n1+n2
+print("concatenated string is:", a)
+
+str=a
+print("Reversed string is:", str[::-1])
+
+str='karthikv'
+print("Sliced string is:", str[2:5])
